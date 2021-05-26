dbd753af974830643f967fc01f384f60a6cda605
v9411 <57716339+v9411@users.noreply.github.com>
2021-05-26 13:22:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I love you LGH, dont kill me 3');
+    res.send('I love you LGH, dont kill me 4');
 });
 
 app.get('/start', function(req, res){
